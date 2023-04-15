aa829232ddf2e1ef0565de0918f60a0509a8128e
romainvairo <romain.vairo@hotmail.com>
2023-04-15 18:25:28 +0200

    update init file http base

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -3,8 +3,8 @@ import logging
 import azure.functions as func
 
 
-def main(req: func.HttpRequest, dfsblob: func.InputStream, blobembed: func.InputStream) -> func.HttpResponse:
-
+#def main(req: func.HttpRequest, dfsblob: func.InputStream, blobembed: func.InputStream) -> func.HttpResponse:
+def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
