492669bf584683d0efb569e1202fb9eb37191cc8
romainvairo <romain.vairo@hotmail.com>
2023-04-21 01:31:35 +0200

    updated file

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -14,25 +14,25 @@ def outlier(col):
     Q3max = Q3 + (1.5 * IQR)
     return Q1min, Q3max
 
-def user_articles_clickes(data ,user_id):
+def articles_clickes_by_user(data ,user_id):
     article_du_user = data.loc[data["user_id"] == user_id]
     return article_du_user
 
-def articles_reco(article_id, indices, cosine_sim2):
-    idx = indices[article_id]
-    sim_scores = list(enumerate(cosine_sim2[idx]))
+def articles_recommandation(article_id, indices_article, sim_cosine):
+    idx = indices_article[article_id]
+    sim_scores = list(enumerate(sim_cosine[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:21]
     return sim_scores
 
-def user_reco(user_id, indices, cosine_sim2, data):
+def user_recommandation(user_id, indices_article, sim_cosine, data):
     logging.info('------------debut  traitement3')
     reco = [] # Création d'un tableau vide 
-    articles_du_user_clickes = user_articles_clickes(data, user_id) # Récupération des articles qui ont été cliquers par un certain utilisateur
+    articles_du_user_clickes = articles_clickes_by_user(data, user_id) # Récupération des articles qui ont été cliquers par un certain utilisateur
     logging.info('------------fin  traitement3')
     logging.info('------------debut  traitement4')
     for art in list(articles_du_user_clickes["article_id"]): # Boucle sur les articles qui ont été cliqué par cet utilisateur
-        article_recommande = articles_reco(art, indices, cosine_sim2) # Cette fonction récupère les articles les plus similaires
+        article_recommande = articles_recommandation(art, indices_article, sim_cosine) # Cette fonction récupère les articles les plus similaires
         reco.append(article_recommande[0]) # Dans le tableau reco les articles les plus similaires y sont ajoutés
     logging.info('------------fin  traitement4')
     logging.info('------------debut  traitement5')
@@ -61,57 +61,57 @@ def recommandation_generator(pca_tranformed_embedding_df, articles,  df, user_id
     articles["words_count"] = np.where(articles["words_count"] > maxScore, maxScore, articles["words_count"])
 
 
-    all_clicks_df = df.join(articles, how='left', on='click_article_id', lsuffix="_1")
+    dataframe_all_clicks = df.join(articles, how='left', on='click_article_id', lsuffix="_1")
 
-    #Create a map to convert article_id to category
+    # Créer une map pour convertir les article_id en category
     dict_article_categories = articles.set_index('article_id')['category_id'].to_dict()
 
-    #Get Categorie associate for each article
-    all_clicks_df['category_id'] = all_clicks_df['click_article_id'].map(dict_article_categories).astype(int)
-    all_clicks_df['total_click'] = all_clicks_df.groupby(['user_id'])['click_article_id'].transform('count')
-    all_clicks_df['total_click_by_category_id'] = all_clicks_df.groupby(['user_id','category_id'])['click_article_id'].transform('count')
-    all_clicks_df['rating'] = all_clicks_df['total_click_by_category_id'] / all_clicks_df['total_click']
+    # Récupère les catégories associées pour chaque article
+    dataframe_all_clicks['category_id'] = dataframe_all_clicks['click_article_id'].map(dict_article_categories).astype(int)
+    dataframe_all_clicks['total_click'] = dataframe_all_clicks.groupby(['user_id'])['click_article_id'].transform('count')
+    dataframe_all_clicks['total_click_by_category_id'] = dataframe_all_clicks.groupby(['user_id','category_id'])['click_article_id'].transform('count')
+    dataframe_all_clicks['rating'] = dataframe_all_clicks['total_click_by_category_id'] / dataframe_all_clicks['total_click']
 
-    all_clicks_df = all_clicks_df.drop_duplicates()
+    dataframe_all_clicks = dataframe_all_clicks.drop_duplicates()
 
-    all_clicks_df = all_clicks_df.drop(['total_click', 'total_click_by_category_id'], axis=1)
+    dataframe_all_clicks = dataframe_all_clicks.drop(['total_click', 'total_click_by_category_id'], axis=1)
 
-    all_clicks_df_sav = all_clicks_df.copy()
+    dataframe_all_clicks_sav = dataframe_all_clicks.copy()
 
-    all_clicks_df = all_clicks_df.drop(['click_article_id'], axis=1)
+    dataframe_all_clicks = dataframe_all_clicks.drop(['click_article_id'], axis=1)
 
-    articles_click_au_moins_une_fois = all_clicks_df_sav.click_article_id.value_counts().index
+    articles_clicks_once = dataframe_all_clicks_sav.click_article_id.value_counts().index
 
-    df_articles_click_au_moins_une_fois = pd.DataFrame(articles_click_au_moins_une_fois).reset_index() 
+    df_articles_clicks_once = pd.DataFrame(articles_clicks_once).reset_index() 
 
-    df_articles_click_au_moins_une_fois.columns =["index", "article_id"]
+    df_articles_clicks_once.columns = ["index", "article_id"]
 
     
 
     # La jointure est faite sur les articles qui ont étés cliqués au moins une fois et sur l'embedding réduit par l'ACP, la jointure se fait sur l'index
-    df_arts_embedd_acp = df_articles_click_au_moins_une_fois.join(pca_tranformed_embedding_df, how='left', on='index', lsuffix="_1")
+    dataframe_arts_embedd_acp = df_articles_clicks_once.join(pca_tranformed_embedding_df, how='left', on='index', lsuffix="_1")
     
-    table_user = (user_articles_clickes(all_clicks_df_sav, 0)).reset_index()
+    user_table = (articles_clickes_by_user(dataframe_all_clicks_sav, user_id)).reset_index()
 
     # suppression des articles déjà cliqués par le user
-    df_arts_embedd_acp_filt = df_arts_embedd_acp[~df_arts_embedd_acp.article_id.isin(table_user.article_id)]
-    arts_embedd_acp = df_arts_embedd_acp_filt.iloc[:, 3:]
+    df_arts_embedd_acp_filt = dataframe_arts_embedd_acp[~dataframe_arts_embedd_acp.article_id.isin(user_table.article_id)]
+    articles_embedd_acp = df_arts_embedd_acp_filt.iloc[:, 3:]
 
-    table_user = table_user[["user_id", "index", "article_id"]]
+    user_table = user_table[["user_id", "index", "article_id"]]
 
-    df_user_merged = table_user.merge(df_arts_embedd_acp, how='left', on='article_id')
+    dataframe_user_merged = user_table.merge(dataframe_arts_embedd_acp, how='left', on='article_id')
     # Les tables qui sont merge sont les cliques de l'utilisateur 0 et l'embedding réduit par l'ACP
 
-    df_user_merged = df_user_merged.iloc[:,5:]
+    dataframe_user_merged = dataframe_user_merged.iloc[:,5:]
     logging.info('------------fin traitement')
     logging.info('------------debut cosinus traitement')
-    cosine_sim2 = cosine_similarity(df_user_merged, arts_embedd_acp)
+    sim_cosine = cosine_similarity(dataframe_user_merged, articles_embedd_acp)
     logging.info('------------fin cosinus traitement')
     logging.info('------------debut  traitement1')
-    titles = table_user["article_id"]
-    indices = pd.Series(range(0,len(table_user["article_id"])), index=titles)
+    titles_article = user_table["article_id"]
+    indices_article = pd.Series(range(0,len(user_table["article_id"])), index=titles_article)
     logging.info('------------fin  traitement1')
-    return user_reco(user_id, indices, cosine_sim2, all_clicks_df_sav)
+    return user_recommandation(user_id, indices_article, sim_cosine, dataframe_all_clicks_sav)
 
 
 def main(req: func.HttpRequest, dfpcablob: func.InputStream, dfartblob: func.InputStream, dfblob: func.InputStream) -> func.HttpResponse:
@@ -135,6 +135,6 @@ def main(req: func.HttpRequest, dfpcablob: func.InputStream, dfartblob: func.Inp
     func.HttpResponse.charset = 'utf-8'
     name = int(name)
     return func.HttpResponse(
-            json.dumps(recommandation_generator(pca_tranformed_embedding_df, articles,  df,name)),
+            json.dumps(recommandation_generator(pca_tranformed_embedding_df, articles, df, name)),
             status_code=200
             )
